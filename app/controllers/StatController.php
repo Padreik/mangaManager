0f85b724d633efa2ad8ade9c7327d0143ddf9199
Phil <girdphil@gmail.com>
2014-07-29 11:23:19 -0400

    Add total cost of collection

--- app/controllers/StatController.php
+++ app/controllers/StatController.php
@@ -4,7 +4,8 @@ class StatController extends BaseController {
     public function index() {
         $parameters = array(
             'nbOfSeries' => \Series::count(),
-            'nbOfMangas' => \Manga::count()
+            'nbOfMangas' => \Manga::count(),
+            'totalPrice' => \Manga::sum('price')
         );
         return View::make('stat.index')->with($parameters);
     }
--- app/views/stat/index.blade.php
+++ app/views/stat/index.blade.php
@@ -9,6 +9,7 @@
         <div class="infosArray list-group">
             {{ DivAsArray::format("Nombre de séries", $nbOfSeries) }}
             {{ DivAsArray::format("Nombre de mangas", $nbOfMangas) }}
+            {{ DivAsArray::format("Coût de la collection", number_format($totalPrice, 2, ',', ' ')." $") }}
         </div>
     </div>
 @stop
